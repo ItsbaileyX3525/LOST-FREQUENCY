91bc29a8d47fb1b6b4c028d4d1dd3758c77abf37
Bailey Miles <baileyleah542@gmail.com>
2025-10-19 14:21:18 +0100

    Add all html files to build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from "vite";
 import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 export default defineConfig({
 	root: "frontend",
 	build: {
 		outDir: "../public",
-		emptyOutDir: true
+		emptyOutDir: true,
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'frontend/index.html'),
+				404: resolve(__dirname, 'frontend/404.html'),
+				page: resolve(__dirname, 'frontend/main.html'),
+				terms: resolve(__dirname, 'frontend/terms.html')
+			}
+		}
 	},
 	server: {
 		port: 5173,
